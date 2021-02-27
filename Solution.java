2a26d259f2f5da9222b66c20a5aae702b007b56d
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-02-26 22:46:23 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Solution.java
@@ -0,0 +1,11 @@
+/* 
+We solemnly publish and declare these variables...
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+    }
+}
